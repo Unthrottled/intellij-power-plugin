814c97c9224b6689f36d65a289fc1532d10d7c36
GitHub Action <action@github.com>
2021-12-03 15:20:53 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/unthrottled/intellijpowerplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.unthrottled.intellijpowerplugin.services
+
+import com.github.unthrottled.intellijpowerplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
